00e992258445efcfd6196e0b7e21f4104b2bd8f5
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-01-22 11:13:11 +0100

    data source equipes.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express"); // import du serveur express
 const app = express();  // instanciation du serveur express
 
-app.listen(80,()=>{
+app.listen(84,()=>{
     console.log("REST API using Node Express JS");
 })
