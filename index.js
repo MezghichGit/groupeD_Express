da4352fc1a8d6b7764d32c9e87b6356ca2b5418e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-01-22 12:02:15 +0100

    request Get All Equipes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express"); // import du serveur express
 const app = express();  // instanciation du serveur express
-
+const equipes = require("./equipes.json");
 app.listen(84,()=>{
     console.log("REST API using Node Express JS");
+})
+
+app.get("/equipes",(request, response)=>{
+    //res.send("Liste des équipes");
+    response.status(200).json(equipes);
 })
